bca8db0862e2f432c98f91a62c99810166deb4e2
qiuyue520 <qiuyue070212@qq.com>
2026-05-10 00:15:07 +0800

    评论后台换回去

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -6,8 +6,8 @@ export const commentConfig: CommentConfig = {
 
 	//twikoo评论系统配置，版本1.7.4
 	twikoo: {
-		// envId: "https://twikoo-xiaonai.netlify.app/.netlify/functions/twikoo",
-		envId: "http://111.170.14.40:26666",//我自己的后台
+		envId: "https://twikoo-xiaonai.netlify.app/.netlify/functions/twikoo",
+		// envId: "http://111.170.14.40:26666",//我自己的后台
 		// 设置 Twikoo 评论系统语言
 		lang: "zh-CN",
 		// 是否启用文章访问量统计功能
